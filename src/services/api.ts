9ea82b849c71ad2b66e50fa2e17eeb811d436d37
Mayron Medeiros <mayron.gomes@grancursosonline.com.br>
2021-07-27 21:45:18 -0300

    feat: Add interceptors

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,27 @@
 import axios from 'axios';
 
-export default axios.create({
-  baseURL: process.env.REACT_APP_API_URI,
+const axiosService = axios.create({
+    baseURL: process.env.REACT_APP_API_URI,
 });
+
+axiosService.interceptors.request.use(
+    async config => {
+        const value = await config
+        console.log(value)
+        return config;
+    },
+    error => {
+        Promise.reject(error)
+    });
+
+axiosService.interceptors.response.use((response) => {
+    console.log(`response interceptor`, response)
+    return response
+}, async function (error) {
+    const originalRequest = error.config;
+    console.log(`error response interceptor`, originalRequest)
+    return Promise.reject(error);
+});
+
+
+export default axiosService;
